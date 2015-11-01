4e5c0cfeb2126234112415711d99ba1e2567b8cb
Pushkar Anand <anandpushkar088@gmail.com>
2015-11-01 15:11:39 +0530

    working on search

--- model/process_search.php
+++ model/process_search.php
@@ -1,4 +1,22 @@
 <?php
 
+if(isset($_GET['term'])){
+	function test_input($data) {
+$servername = "localhost";
+$username = "Ed-Free";
+$password = "password@me";
+$dbname = "edfree";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  $data = mysqli_real_escape_string($conn,$data);
+  $data = strip_tags($data);
+  return $data;
+}
+	$searchTerm = test_input($_GET['term']);
+}
+
 
 ?>
