e7c7930b6cff3759bdf8b6f3c6363db7d23d1dc5
Pushkar Anand <anandpushkar088@gmail.com>
2015-11-01 15:31:33 +0530

    latest

--- model/process_search.php
+++ model/process_search.php
@@ -63,14 +63,15 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 				<div id='MicrosoftTranslatorWidget' class='Dark' style='color:white;background-color:#555555'></div><script type='text/javascript'>setTimeout(function(){{var s=document.createElement('script');s.type='text/javascript';s.charset='UTF-8';s.src=((location && location.href && location.href.indexOf('https') == 0)?'https://ssl.microsofttranslator.com':'http://www.microsofttranslator.com')+'/ajax/v3/WidgetV3.ashx?siteData=ueOIGRSKkd965FeEGM5JtQ**&ctf=True&ui=true&settings=Manual&from=';var p=document.getElementsByTagName('head')[0]||document.documentElement;p.insertBefore(s,p.firstChild); }},0);</script>
 				<div class="clearfix"></div>
 			</div>
-			
-		</div>
 		<div class='container'>
 		<h2>Search Results</h2>
 
 
 
 	</div>
+			
+		</div>
+
 
 	</div>
 
