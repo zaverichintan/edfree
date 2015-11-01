a0246dfd50ba7e20a95feceb8b506b3fea4a1e68
Pushkar Anand <anandpushkar088@gmail.com>
2015-11-01 12:21:28 +0530

    edits

--- model/crawl.class.php
+++ model/crawl.class.php
@@ -2,13 +2,28 @@
 require_once('simple_html_dom');
 
 class Crawl{
-	private url;
+	private $url;
+	private $videoLink
 	public function __construct($u) {
 		$this->url = $u;
 	}
 
 	public getVideoLink(){
+
+		$u = $this->url;
+		$data = parse_url($u);
+
+		$domain = $parse['host'];
+
+		if($domain=='youtube.com'||$domain=='youtube.co.in'){
+			$this->videoLink = $this->url;
+		}
+	else{
 		
+	}
+
+
+		return $this->videoLink;
 	}
 }
 
